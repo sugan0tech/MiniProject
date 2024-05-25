8d01dedff1d7358583e4200e5e9ea6f29ef155da
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-05-25 13:59:36 +0530

    fix: rernamed typo staff table name

--- Matrimony/MatrimonyApiService/Migrations/20240525082850_Staff-Table-Rename.Designer.cs
+++ Matrimony/MatrimonyApiService/Migrations/20240525082850_Staff-Table-Rename.Designer.cs
@@ -4,6 +4,7 @@ using MatrimonyApiService.Commons;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MatrimonyApiService.Migrations
 {
     [DbContext(typeof(MatrimonyContext))]
-    partial class MatrimonyContextModelSnapshot : ModelSnapshot
+    [Migration("20240525082850_Staff-Table-Rename")]
+    partial class StaffTableRename
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Matrimony/MatrimonyApiService/Migrations/20240525082850_Staff-Table-Rename.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatrimonyApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class StaffTableRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staves_Addresses_AddressId1",
+                table: "Staves");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Staves",
+                table: "Staves");
+
+            migrationBuilder.RenameTable(
+                name: "Staves",
+                newName: "Staffs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Staves_AddressId1",
+                table: "Staffs",
+                newName: "IX_Staffs_AddressId1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Religion",
+                table: "Preferences",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Occupation",
+                table: "Preferences",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MotherTongue",
+                table: "Preferences",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Education",
+                table: "Preferences",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Staffs",
+                table: "Staffs",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staffs_Addresses_AddressId1",
+                table: "Staffs",
+                column: "AddressId1",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staffs_Addresses_AddressId1",
+                table: "Staffs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Staffs",
+                table: "Staffs");
+
+            migrationBuilder.RenameTable(
+                name: "Staffs",
+                newName: "Staves");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Staffs_AddressId1",
+                table: "Staves",
+                newName: "IX_Staves_AddressId1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Religion",
+                table: "Preferences",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Occupation",
+                table: "Preferences",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MotherTongue",
+                table: "Preferences",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Education",
+                table: "Preferences",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Staves",
+                table: "Staves",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staves_Addresses_AddressId1",
+                table: "Staves",
+                column: "AddressId1",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+    }
+}
